b04e196363186a51bc45b5c8e640d40465864c25
Nezuko1909 <kny1909.nezuko@gmail.com>
2024-10-16 13:19:12 +0700

    Update library2: Add menthod

--- src/main/java/com/library/document/Document.java
+++ src/main/java/com/library/document/Document.java
@@ -5,14 +5,12 @@ class Document {
     private String ID; 
     private String Name;
     private String Group;
-    private int Quantity;
     private boolean Available;
 
     public Document() {
         this.ID = generateID();
         this.Name = "No name";
         this.Group = "Default";
-        this.Quantity = 0;
         this.Available = false;
     }
 
@@ -20,8 +18,7 @@ class Document {
         this.ID = generateID(); 
         this.Name = name;
         this.Group = group;
-        this.Quantity = quantity;
-        this.Available = quantity > 0;
+        this.Available = true;
     }
 
     // ID Auto generate, Example: 000000001, 012345678,...
@@ -51,15 +48,11 @@ class Document {
         this.Group = group;
     }
 
-    public int getQuantity() {
-        return Quantity;
+    public void setAvailable(boolean isAvailable) {
+        this.Available = isAvailable;
     }
 
-    public void setQuantity(int quantity) {
-        this.Quantity = quantity;
-    }
-
-    public boolean isAvailable() {
+    public boolean getAvailable() {
         return Available;
     }
 }
--- src/main/java/com/library/library/Library2.java
+++ src/main/java/com/library/library/Library2.java
@@ -17,29 +17,29 @@ public class Library2 {
         createDataBase();
         // Create listBooks
         createList("CREATE TABLE IF NOT EXISTS Books ("
-                    + "ID VARCHAR(255) PRIMARY KEY, "
-                    + "Name VARCHAR(255), "
-                    + "Group VARCHAR(50), "
-                    + "Author VARCHAR(255), "
+                    + "id VARCHAR(255) PRIMARY KEY, "
+                    + "name VARCHAR(255), "
+                    + "group VARCHAR(50), "
+                    + "author VARCHAR(255), "
                     + "isAvailable BOOLEAN)");
 
         // Create listMagazines
         createList("CREATE TABLE IF NOT EXISTS Magazines ("
-                    + "ID VARCHAR(255) PRIMARY KEY, "
-                    + "Name VARCHAR(255), "
-                    + "Group VARCHAR(50), "
-                    + "Publisher VARCHAR(255), "
-                    + "Genre VARCHAR(255), "
+                    + "id VARCHAR(255) PRIMARY KEY, "
+                    + "name VARCHAR(255), "
+                    + "group VARCHAR(50), "
+                    + "publisher VARCHAR(255), "
+                    + "genre VARCHAR(255), "
                     + "isAvailable BOOLEAN)");
 
         // Create listNewspaper
         createList("CREATE TABLE IF NOT EXISTS Newspaper ("
-                            + "ID VARCHAR(255) PRIMARY KEY, "
-                            + "Name VARCHAR(255), "
-                            + "Group VARCHAR(50), "
-                            + "Source VARCHAR(255), "
-                            + "Category VARCHAR(255), "
-                            + "Region VARCHAR(255), "
+                            + "id VARCHAR(255) PRIMARY KEY, "
+                            + "name VARCHAR(255), "
+                            + "group VARCHAR(50), "
+                            + "source VARCHAR(255), "
+                            + "category VARCHAR(255), "
+                            + "region VARCHAR(255), "
                             + "isAvailable BOOLEAN)");
     }
 
@@ -72,14 +72,17 @@ public class Library2 {
      * @param book The new book to add.
      */
     public void addDocuments(Book book) {
-        String sql_statement = "INSERT INTO Books (ID, Name, Group, Author) VALUES (?, ?, ?, ?)";
+        String sql_statement = "INSERT INTO Books " 
+                               + "(id, name, group, author, isAvailable) " 
+                               + "VALUES (?, ?, ?, ?, ?)";
 
-        try (Connection conn = DriverManager.getConnection(url);
+        try (Connection conn = DriverManager.getConnection(url); 
              PreparedStatement pstmt = conn.prepareStatement(sql_statement)) {
             pstmt.setString(1, book.getID());
             pstmt.setString(2, book.getName());
             pstmt.setString(3, book.getGroup());
             pstmt.setString(4, book.getAuthor());
+            pstmt.setBoolean(5, book.getAvailable());
             pstmt.executeUpdate();
             System.out.println("Data inserted successfully");
         } catch (SQLException e) {
@@ -88,13 +91,45 @@ public class Library2 {
     }
 
     public void addDocuments(Magazine mgz) {
+        String sql_statement = "INSERT INTO Magazines "
+                               + "(id, name, group, publisher, genre, isAvailable) " 
+                               + "VALUES (?, ?, ?, ?, ?, ?)";
 
+        try (Connection conn = DriverManager.getConnection(url);
+             PreparedStatement pstmt = conn.prepareStatement(sql_statement)) {
+                pstmt.setString(1, mgz.getID());
+                pstmt.setString(2, mgz.getName());
+                pstmt.setString(3, mgz.getGroup());
+                pstmt.setString(4, mgz.getPublisher());
+                pstmt.setString(5, mgz.getGenre());
+                pstmt.setBoolean(6, mgz.getAvailable());
+                pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public void addDocuments(Newspaper news) {
 
     }
 
+    /**Update Document
+     * 
+     */
+    public void updateDocuments(Book book) {
+
+    }
+
+    public void updateDocuments(Magazine mgz) {
+        
+    }
+
+    public void updateDocuments(Newspaper news) {
+        
+    }
+
+
+    /**Remove Document */
     public void removeDocument(Book book) {
         String sql_statement = "DELETE FROM Books WHERE id = ?";
 
