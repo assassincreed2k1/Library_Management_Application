0474b9042439efa150617d727fc1ad4820d746e5
ChauKhanhLy <ckhanhly2209@gmail.com>
2024-10-21 16:04:37 +0700

    new commit

---
+++ src/main/java/com/library/Librarian.java
@@ -0,0 +1,50 @@
+package com.library;
+
+public class Librarian extends Person {
+    private String employeeId;
+    private String position;
+
+    /**
+     * COnstructor for Librarian with all information
+     * @param name
+     * @param address
+     * @param gender
+     * @param dateOfBirth
+     * @param phoneNumber
+     * @param email
+     * @param employeeId
+     * @param position la chuc vu
+     */
+    public Librarian(String name, String address, String gender, String dateOfBirth, String phoneNumber, String email, String employeeId, String position){
+        super(name, address, gender, dateOfBirth, phoneNumber, email)
+        this.employeeId = employeeId;
+        this.position = position;
+    }
+
+    public String getEmployeeId(){
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId){
+        this.employeeId = employeeId;
+    }
+
+    public String getPosition(){
+        return position;
+    }
+
+    public void setPosition(String position){
+        this.position = position;
+    }
+
+    /**
+     * Override getDetails to include librarian-specific info
+     * @return String with all details
+     */
+    @Override
+    public String getDetails() {
+        return String.format("%sEmployee ID: %s\nPosition: %s\n",
+         super.getDetails(), employeeId, position);
+    }
+}
+
