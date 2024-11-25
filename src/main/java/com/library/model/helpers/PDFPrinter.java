72205d58289d580e4517b5fbb4546a37367c59dc
nghia <assassincreed2k1@gmail.com>
2024-11-25 15:48:36 +0700

    Add javadoc

--- src/main/java/com/library/model/helpers/PDFPrinter.java
+++ src/main/java/com/library/model/helpers/PDFPrinter.java
@@ -5,7 +5,7 @@ import javafx.scene.layout.VBox;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.font.PDType0Font;
 
 import java.io.File;
 import java.io.IOException;
@@ -22,8 +22,10 @@ public class PDFPrinter {
     
         try {
             // Prepare content stream in a try-with-resources block
+            PDType0Font font = PDType0Font.load(document, new File("/font/DejaVuSans-Bold.ttf"));
+
             try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
-                contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
+                contentStream.setFont(font, 12);
                 contentStream.beginText();
                 contentStream.newLineAtOffset(50, 750);
     
